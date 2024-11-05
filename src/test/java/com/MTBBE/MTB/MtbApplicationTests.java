5bc99d9c35d32717d402b0954861a7555418d832
kalab Afera <kalabafex@gmail.com>
2024-11-05 15:39:21 -0600

    init

---
+++ src/test/java/com/MTBBE/MTB/MtbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MTBBE.MTB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
